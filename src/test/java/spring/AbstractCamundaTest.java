567d8769ea83b7353e5b4555b8506ba39706c746
Albert van veen <albert@qcoding.nl>
2018-05-14 10:43:34 +0200

    first commit

---
+++ src/test/java/spring/AbstractCamundaTest.java
@@ -0,0 +1,35 @@
+package spring;
+
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.PostConstruct;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = { InMemProcessEngineConfiguration.class })
+public abstract class AbstractCamundaTest {
+
+
+    @Autowired
+    ProcessEngine processEngine;
+
+    protected ProcessInstance processInstance;
+
+    @Rule
+    @ClassRule
+    public static ProcessEngineRule rule;
+
+    @PostConstruct
+    void initRule() {
+        rule = TestCoverageProcessEngineRuleBuilder.create(processEngine).build();
+    }
+
+}
---
+++ src/test/java/spring/SpringProcessWithCoverageTest.java
@@ -0,0 +1,38 @@
+package spring;
+
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.test.Deployment;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Deployment(resources="process.bpmn")
+public class SpringProcessWithCoverageTest extends AbstractCamundaTest {
+
+  @Test
+  public void start_and_finish_process() {
+
+    Map<String, Object> variables = new HashMap<String, Object>();
+    variables.put("path", "A");
+    final ProcessInstance processInstance = processEngine.getRuntimeService().startProcessInstanceByKey("process-test-coverage", variables);
+
+    Assert.assertNotNull(processInstance);
+    Assert.assertTrue(processInstance.isEnded());
+
+  }
+  @Test
+  public void testB() {
+
+    Map<String, Object> variables = new HashMap<String, Object>();
+    variables.put("path", "A");
+    final ProcessInstance processInstance = processEngine.getRuntimeService().startProcessInstanceByKey("process-test-coverage", variables);
+
+    Assert.assertNotNull(processInstance);
+    Assert.assertTrue(processInstance.isEnded());
+
+  }
+
+}
